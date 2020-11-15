d7870c3c69881fea0f41d67bc054cb86acbb510c
Adrian Jeż <root.hqapps@gmail.com>
2020-11-15 12:15:31 +0100

    FTP

--- src/main/kotlin/com/journaler/api/controller/PhotosController.kt
+++ src/main/kotlin/com/journaler/api/controller/PhotosController.kt
@@ -3,12 +3,14 @@ package com.journaler.api.controller
 import com.journaler.api.data.PhotoEntity
 import com.journaler.api.data.PhotoEntityDTO
 import com.journaler.api.repository.PhotoRepository
+import org.apache.commons.net.ftp.FTPClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
+import javax.print.attribute.standard.ReferenceUriSchemesSupported.FILE
 
 @RestController
 class PhotosController {
@@ -19,36 +21,31 @@ class PhotosController {
     @PostMapping("/uploadPhoto")
     fun uploadPhoto(@RequestParam("file") file: MultipartFile): PhotoEntityDTO? {
 
-        /*
-        val FTP_ADDRESS = "your_address"
-        val LOGIN = "you_login"
-        val PSW = "you_password"
+
+        val FTP_ADDRESS = "files.000webhost.com"
+        val LOGIN = "portalogloszen2"
+        val PSW = "svbJdgIb(qz3fW%xG)pr"
 
         var con: FTPClient? = null
-        var newFileName = UUID.randomUUID()
+        var newFileName = UUID.randomUUID().toString() + ".png"
         try {
             con = FTPClient()
             con.connect(FTP_ADDRESS)
             if (con.login(LOGIN, PSW)) {
                 con.enterLocalPassiveMode() // important!
-                con.setFileType(FILE.value)
-                val result: Boolean = con.storeFile(newFileName.toString(), file.inputStream)
+                con.setFileType(FTPClient.BINARY_FILE_TYPE)
+                val result: Boolean = con.storeFile("/public_html/$newFileName", file.inputStream)
                 con.logout()
                 con.disconnect()
-                return null
             }
         } catch (e: Exception) {
             e.printStackTrace()
             return null
         }
-
-         */
-            // We have URL https://hqapps.ddns.net/
         return PhotoEntityDTO(
                 photoRepository.save(
                         PhotoEntity(
-                                url = "https://hqapps.ddns.net/" + UUID.randomUUID().toString()
-                                //FTP_ADDRESS + File.separator + newFileName.toString()
+                                url = "https://portalogloszen2.000webhostapp.com/$newFileName"
                         )
                 ))
     }
